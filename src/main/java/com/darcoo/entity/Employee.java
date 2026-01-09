e85cf384cc430bc1fbdd4134c05930b1a3890e9a
Nitin8795 <vishwakarmanitin987@gmail.com>
2026-01-09 16:46:54 +0530

    update

--- src/main/java/com/darcoo/entity/Employee.java
+++ src/main/java/com/darcoo/entity/Employee.java
@@ -5,10 +5,6 @@ public class Employee {
 	private int id; 
 	private String name,gender;
 	private int salary;
-	public Employee() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
 
 	
 	public int getId() {
@@ -57,9 +53,8 @@ public class Employee {
 		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
 	}
 
-	public Employee(int id, String name, String gender, int salary) {
+	public Employee(String name, String gender, int salary) {
 		super();
-		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.salary = salary;
--- src/main/java/com/darcoo/main/EmployeeRunner.java
+++ src/main/java/com/darcoo/main/EmployeeRunner.java
@@ -1,8 +1,13 @@
 package com.darcoo.main;
 
 import org.hibernate.Session;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import com.darcoo.entity.Employee;
@@ -12,14 +17,31 @@ public class EmployeeRunner {
 	
 	public static void main(String[]args) {
 		
-		Employee emp = new Employee(1,"vinod","Male",33333);
+		Employee emp = new Employee("Shivam","male",90000);
 		
-		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-	    SessionFactory sesionFactory	= cfg.buildSessionFactory();
-	    Session session= sesionFactory.openSession();
+//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+//	    SessionFactory sessionFactory= cfg.buildSessionFactory();
+	  		
+//		method chain single line.......
+		
+//	    SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	    
+//		2 method 
+		
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+//		Metadata metadata = new MetadataSources (ssr).getMetadataBuilder().build();
+//	    SessionFactory sessionFactory = metadata.buildSessionFactory();
+	
+//		2 method chaining single line
+		
+		SessionFactory sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder()
+		.configure("hibernate.cfg.xml").build()).getMetadataBuilder()
+		.build().buildSessionFactory();	    
+	    
+	    
+	    Session session= sessionFactory.openSession();
 	    Transaction tx = session.beginTransaction();
-	    
-	    
+	   
 	    session.persist(emp);
 	    tx.commit();
 	    
