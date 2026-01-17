52236effebf778a1b2afa3efb678f0e3468aa062
Nitin8795 <vishwakarmanitin987@gmail.com>
2026-01-18 02:22:13 +0530

    update

---
+++ src/main/java/com/darcoo/Config/EmpConfiguration.java
@@ -0,0 +1,45 @@
+package com.darcoo.Config;
+
+	import java.util.Properties;
+
+	import org.hibernate.SessionFactory;
+	import org.hibernate.boot.Metadata;
+	import org.hibernate.boot.MetadataSources;
+	import org.hibernate.boot.registry.StandardServiceRegistry;
+	import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+	import org.hibernate.cfg.Environment;
+
+	import com.darcoo.entity.Employee;
+
+	public class EmpConfiguration {
+	
+		    public static SessionFactory getSessionFactory() {
+
+		     
+
+		            Properties properties = new Properties();
+
+		            properties .put(Environment.JAKARTA_JDBC_URL,"jdbc:mysql://localhost:3306/hibernate");
+		            
+		            properties .put(Environment.JAKARTA_JDBC_USER, "root");
+		            properties .put(Environment.JAKARTA_JDBC_PASSWORD, "root");
+		            properties .put(Environment.HBM2DDL_AUTO, "create"); // table create
+		            properties .put(Environment.SHOW_SQL, "true");
+		            properties .put(Environment.FORMAT_SQL, "true");
+//
+//		            StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(properties ).build();
+//		            Metadata metadata = new MetadataSources(ssr).addAnnotatedClass(com.darcoo.entity.Employee.class).getMetadataBuilder().build();
+//		            SessionFactory sessionFactory = metadata.buildSessionFactory();
+		            
+//		            return sessionFactory;
+
+		            
+//		            single line method chaining
+		            
+		           return new MetadataSources(new StandardServiceRegistryBuilder().applySettings(properties ).build()).addAnnotatedClass(com.darcoo.entity.Employee.class).getMetadataBuilder().build().buildSessionFactory();
+		            
+		            
+		            
+		            
+		    }	
+}
--- src/main/java/com/darcoo/main/EmployeeRunner.java
+++ src/main/java/com/darcoo/main/EmployeeRunner.java
@@ -10,6 +10,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import com.darcoo.Config.EmpConfiguration;
 import com.darcoo.entity.Employee;
 
 
@@ -17,29 +18,10 @@ public class EmployeeRunner {
 	
 	public static void main(String[]args) {
 		
-		Employee emp = new Employee("Shivam","male",90000);
+		Employee emp = new Employee("Akash","male",90000);
 		
-//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
-//	    SessionFactory sessionFactory= cfg.buildSessionFactory();
-	  		
-//		method chain single line.......
 		
-//	    SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-	    
-//		2 method 
-		
-//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-//		Metadata metadata = new MetadataSources (ssr).getMetadataBuilder().build();
-//	    SessionFactory sessionFactory = metadata.buildSessionFactory();
-	
-//		2 method chaining single line
-		
-		SessionFactory sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder()
-		.configure("hibernate.cfg.xml").build()).getMetadataBuilder()
-		.build().buildSessionFactory();	    
-	    
-	    
-	    Session session= sessionFactory.openSession();
+		Session session = EmpConfiguration.getSessionFactory().openSession();
 	    Transaction tx = session.beginTransaction();
 	   
 	    session.persist(emp);
